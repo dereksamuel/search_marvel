648ef8d97939d973ad91c3f16c1b0e21c2eb450a
Derek Paul <dereksamuelgr@gmail.com>
2021-03-12 10:35:12 -0500

    hotjar vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/fontAwesome'
+import Hotjar from 'vue-hotjar'
 
+Vue.use(Hotjar, {
+  id: 'https://604a39018b1085142ceef115--elastic-leavitt-9379ad.netlify.app/',
+  isProduction: true
+})
 createApp(App).use(store).use(router).mount('#app')
